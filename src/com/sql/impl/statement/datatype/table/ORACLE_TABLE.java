9863bed4686b3ee0a5b27636bb3559e9c4a55cc0
DougLei <953034206@qq.com>
2019-01-25 17:33:07 +0800

    t

--- src/com/sql/impl/statement/datatype/table/ORACLE_TABLE.java
+++ src/com/sql/impl/statement/datatype/table/ORACLE_TABLE.java
@@ -1,5 +1,6 @@
 package com.sql.impl.statement.datatype.table;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.sql.impl.statement.datatype.AbstractCustomDataType;
 import com.sql.impl.statement.datatype.DataType;
@@ -17,18 +18,35 @@ public class ORACLE_TABLE extends AbstractCustomDataType{
 	
 
 	public String getAppendCustomSqlStatement(JSONObject customJson) {
-		return null;
+		return customJson.getString("typeName");
 	}
 
 	protected DataType getCustomDataType() {
 		return DataType.TABLE;
 	}
 
-
-	@Override
 	protected String getCreateTypeSql(JSONObject customJson) {
-		// TODO Auto-generated method stub
-		return null;
+		StringBuilder sb = new StringBuilder(800);
+		String typeName = customJson.getString("typeName");
+		sb.append("create type ").append(typeName).append("_object").append(" as object(").append(newline());
+		
+		JSONArray array = customJson.getJSONArray("column");
+		JSONObject json = null;
+		for(int i=0;i<array.size();i++){
+			json = array.getJSONObject(i);
+			sb.append(json.getString("name")).append(" ").append(DataType.toValue(json.getString("dataType")).getDataType());
+			if(json.getIntValue("length")> 0){
+				sb.append("(").append(json.getIntValue("length")).append(")");
+			}
+			if(i<array.size()-1){
+				sb.append(",");
+			}
+			sb.append(newline());
+		}
+		sb.append(");").append(newline());
+		
+		sb.append("create type ").append(typeName).append(" as table of ").append(typeName).append("_object");
+		return sb.toString();
 	}
 
 	public boolean isSupportCreateType() {
