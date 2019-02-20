a47fd46a6f80daf5e75720631274448664ad52bb
DougLei <953034206@qq.com>
2019-02-20 15:34:51 +0800

    t

--- src/com/sql/impl/statement/complex/object/procedure/model/step/entity/logic/cursorloop/SQLSERVER_CURSORLOOP.java
+++ src/com/sql/impl/statement/complex/object/procedure/model/step/entity/logic/cursorloop/SQLSERVER_CURSORLOOP.java
@@ -2,24 +2,37 @@ package com.sql.impl.statement.complex.object.procedure.model.step.entity.logic.
 
 import java.util.List;
 
-import com.sql.impl.statement.complex.object.procedure.model.step.entity.logic.LogicEntity;
-
 /**
  * 
  * @author DougLei
  */
-public class SQLSERVER_CURSORLOOP extends LogicEntity{
-
+public class SQLSERVER_CURSORLOOP extends AbstractCursorLoop{
+	
 	public SQLSERVER_CURSORLOOP(String cursorName, List<String> variableNameList) {
-		super(null, false);
+		super(cursorName, variableNameList);
 	}
 
 	public String getSqlStatement(boolean isEnd, String sqlStatement) {
 		StringBuilder sb = buildSqlStringBuilder(sqlStatement);
-		sb.append("while ").append(conditionSqlStatement).append(newline());
+		sb.append("open ").append(cursorName).append(newline());
+		sb.append("fetch next from ").append(cursorName).append(" into ").append(getVariableNames()).append(newline());
+		sb.append("while @@fetch_status = 0").append(newline());
 		sb.append("begin").append(newline());
 		sb.append(sqlStatement).append(newline());
 		sb.append("end").append(newline());
+		sb.append("close ").append(cursorName).append(newline());
+		sb.append("deallocate ").append(cursorName).append(newline());
+		return sb.toString();
+	}
+
+	protected String getVariableNames() {
+		StringBuilder sb = new StringBuilder();
+		for(int i=0;i<variableNameList.size();i++){
+			sb.append("@").append(variableNameList.get(i));
+			if(i < variableNameList.size()-1){
+				sb.append(", ");
+			}
+		}
 		return sb.toString();
 	}
 }
