bed87e8e68208bb75eabb3c2811c0c23ceb08ae9
DougLei <953034206@qq.com>
2019-04-08 10:25:35 +0800

    t

---
+++ resources/busi/procedure/WithSql.sql
@@ -0,0 +1,52 @@
+WITH cte(ID,typename,PARENT_ID)
+AS 
+(
+SELECT 
+ID,
+VALUESNAME AS TYPENAME,
+NULL AS PARENT_ID
+FROM V_MDMDATA 
+WHERE DATADICTIONARYTYPECODE = '15' AND RECORDSTATUS = 1
+UNION ALL 
+select 
+id,
+typename,
+PARENT_ID
+from MDM_CHECKELEMENTTYPE
+),
+A AS
+(
+SELECT 
+ID,
+typename,
+PARENT_ID 
+FROM cte 
+WHERE ID= $Id$
+UNION ALL
+SELECT 
+cte.ID, 
+cte.typename, 
+cte.PARENT_ID 
+FROM cte
+INNER JOIN A ON cte.PARENT_ID = A.ID
+)
+select 
+id,
+elementtypeid,
+elementcode,
+elementname,
+case when enabled = 3 then '起草'
+when enabled = 1 then '启用'
+when enabled = 2 then '禁用'
+end as enabledtext,
+case when checkmethod = 3 then '检查'
+when checkmethod = 1 then '测量'
+when checkmethod = 2 then '称重'
+end as checkmethodtext,
+case when  recordformat= 1 then '单值'
+when recordformat = 2 then '表格值'
+end as recordformattext,
+remark,
+create_date
+from MDM_CHECKELEMENTINFO 
+where elementtypeid in (SELECT ID FROM A)
---
+++ src/com/test/busi/procedure/WithSql.java
@@ -0,0 +1,12 @@
+package com.test.busi.procedure;
+
+import com.sql.DBSqlStatementBuilder;
+import com.sql.enums.DatabaseType;
+
+public class WithSql {
+	public static void main(String[] args) {
+		DBSqlStatementBuilder builder = new DBSqlStatementBuilder(DatabaseType.SQLSERVER);
+		String sql = builder.buildSqlStatementByFile("resources/busi/procedure/WithSql.json");
+		System.out.println(sql);
+	}
+}
